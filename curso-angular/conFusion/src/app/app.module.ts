04fb4b1b7210a402893e1b12f462b5a277cfb627
AndresBettinelli <abettinelli@baufest.com>
2020-01-28 17:20:42 -0300

    Components Part 2 - curso angular

--- curso-angular/conFusion/src/app/app.module.ts
+++ curso-angular/conFusion/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { AppComponent } from './app.component';
-import 'hammerjs';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+import 'hammerjs';
 
 //componentes propios
+import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
@@ -20,7 +24,10 @@ import { MenuComponent } from './menu/menu.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
