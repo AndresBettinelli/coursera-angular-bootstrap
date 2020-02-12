c2ba4877aa81aad12d601957ebca3e41c9f92ca9
AndresBettinelli <abettinelli@baufest.com>
2020-02-12 18:14:59 -0300

    Single Page Applications Part 2 - Angular semana 2

--- curso-angular/conFusion/src/app/app-routing/routes.ts
+++ curso-angular/conFusion/src/app/app-routing/routes.ts
@@ -8,6 +8,7 @@ import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes = [
   { path: 'contactus',     component: ContactComponent },
+  { path: 'dishdetail/:id',     component: DishdetailComponent },
   { path: 'home',  component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
