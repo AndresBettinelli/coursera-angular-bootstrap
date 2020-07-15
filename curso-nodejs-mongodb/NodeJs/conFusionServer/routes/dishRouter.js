1ff8b2ffa1ca391c9753e8d6b148434bf4481ab4
AndresBettinelli <abettinelli@baufest.com>
2020-07-15 20:35:50 -0300

    Express REST API with MongoDB and Mongoose Part 2 - NodeJs Semana 2

--- curso-nodejs-mongodb/NodeJs/conFusionServer/routes/dishRouter.js
+++ curso-nodejs-mongodb/NodeJs/conFusionServer/routes/dishRouter.js
@@ -8,6 +8,41 @@ const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
+//dishes sin id
+dishRouter.route('/')
+.get((req,res,next) => {
+    Dishes.find({})
+    .then((dishes) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dishes);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post((req, res, next) => {
+    Dishes.create(req.body)
+    .then((dish) => {
+        console.log('Dish Created ', dish);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes');
+})
+.delete((req, res, next) => {
+    Dishes.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));    
+});
+// dishes con id
 dishRouter.route('/:dishId/comments')
 .get((req,res,next) => {
     Dishes.findById(req.params.dishId)
