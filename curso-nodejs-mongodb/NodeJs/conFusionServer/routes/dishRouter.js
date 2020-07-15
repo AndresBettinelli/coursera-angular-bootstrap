04520665c0073436c20a866b0f089d083e640167
AndresBettinelli <abettinelli@baufest.com>
2020-07-15 20:40:41 -0300

    Express REST API with MongoDB and Mongoose Part 2 - NodeJs Semana 2

--- curso-nodejs-mongodb/NodeJs/conFusionServer/routes/dishRouter.js
+++ curso-nodejs-mongodb/NodeJs/conFusionServer/routes/dishRouter.js
@@ -43,6 +43,41 @@ dishRouter.route('/')
     .catch((err) => next(err));    
 });
 // dishes con id
+dishRouter.route('/:dishId')
+.get((req,res,next) => {
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+})
+.put((req, res, next) => {
+    Dishes.findByIdAndUpdate(req.params.dishId, {
+        $set: req.body
+    }, { new: true })
+    .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.delete((req, res, next) => {
+    Dishes.findByIdAndRemove(req.params.dishId)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
 dishRouter.route('/:dishId/comments')
 .get((req,res,next) => {
     Dishes.findById(req.params.dishId)
